660cd4f770bd013fc6db1482edfd79b1da8af8dd
jpnouchi <jpnouchi@gmail.com>
2013-04-23 01:52:06 -0500

    Avance grabar ContentProvider

--- ProyectoAndroid/src/com/jpnouchi/proyectoandroid/MainActivity.java
+++ ProyectoAndroid/src/com/jpnouchi/proyectoandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.jpnouchi.proyectoandroid;
 
+import java.lang.annotation.ElementType;
 import java.util.List;
 
+import android.content.ContentValues;
+import android.net.Uri;
+import android.widget.Toast;
 import com.jpnouchi.proyectoandroid.model.SmartPhone;
 import com.jpnouchi.proyectoandroid.parser.SmartPhoneParser;
 import com.jpnouchi.proyectoandroid.parser.impl.SmartPhoneParserImpl;
@@ -13,12 +17,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+import com.jpnouchi.proyectoandroid.utilitario.Constantes;
 
 public class MainActivity extends Activity {
 
 	private TextView txtviewSmartphonesResult;
 	private XmlResourceParser rscParser;
 	private SmartPhoneParser smartPhoneParserImpl;
+    private   List <SmartPhone> listSmartPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,24 +53,49 @@ public class MainActivity extends Activity {
 //		txtviewSmartphonesResult.setText("");
         rscParser =this.getResources().getXml(R.xml.phone_android);
 		smartPhoneParserImpl = new SmartPhoneParserImpl(rscParser);
-        List <SmartPhone> listaSmartPhone=smartPhoneParserImpl.parse();
+        listSmartPhone=smartPhoneParserImpl.parse();
 		
-		if(listaSmartPhone!=null){
-			StringBuilder texto=new StringBuilder();
-			for(SmartPhone smartPhone:listaSmartPhone){
-				texto.append("");
-				texto.append(smartPhone.getBrand());
-				texto.append(" ");
-				texto.append(smartPhone.getModel());
-				texto.append("\n");
+		if(listSmartPhone!=null){
+			StringBuilder text=new StringBuilder();
+			for(SmartPhone smartPhone:listSmartPhone){
+				text.append("");
+				text.append(smartPhone.getBrand());
+				text.append(" ");
+				text.append(smartPhone.getModel());
+				text.append("\n");
 			}
-			txtviewSmartphonesResult.setText(texto.toString());
+			txtviewSmartphonesResult.setText(text.toString());
 		}
-		
-		
 
 	}
-    
+
+
+    public void onclickGrabarContentProvider(View v){
+        int main = Log.d("main", "onclickGrabarContentProvider " + listSmartPhone);
+
+        ContentValues values;
+        for(SmartPhone smartPhone:listSmartPhone){
+            values=phone2ContentValues(smartPhone);
+            Uri uri = getContentResolver().insert(Uri.parse(Constantes.PROVIDER_NAME),values);
+            Toast.makeText(getBaseContext(), uri.toString(),Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private ContentValues phone2ContentValues(SmartPhone beanSmartPhone){
+        ContentValues values = new ContentValues();
+        values.put(Constantes.COL_MANUFACTURER,beanSmartPhone.getManufacturer());
+        values.put(Constantes.COL_BRAND,beanSmartPhone.getBrand());
+        values.put(Constantes.COL_MODEL,beanSmartPhone.getModel());
+        values.put(Constantes.COL_RELEASE,beanSmartPhone.getRelease());
+        values.put(Constantes.COL_OS,beanSmartPhone.getOs());
+        values.put(Constantes.COL_VERSION,beanSmartPhone.getOsVersion());
+        values.put(Constantes.COL_PROCESSOR,beanSmartPhone.getProcessor());
+        values.put(Constantes.COL_MEMORY,beanSmartPhone.getMemory());
+        values.put(Constantes.COL_STORAGE,beanSmartPhone.getStorage());
+        values.put(Constantes.COL_WEIGHT,beanSmartPhone.getWeight());
+        return values;
+    }
     
     
 }
--- ProyectoAndroid/src/com/jpnouchi/proyectoandroid/utilitario/Constantes.java
+++ ProyectoAndroid/src/com/jpnouchi/proyectoandroid/utilitario/Constantes.java
@@ -13,6 +13,22 @@ public class Constantes {
 	public static final String TAG_MEMORY ="memory";
 	public static final String TAG_STORAGE ="storage";
 	public static final String TAG_WEIGHT ="weight";
+
+    public static final String COL_MANUFACTURER ="manufacturer";
+    public static final String COL_BRAND ="brand";
+    public static final String COL_MODEL ="model";
+    public static final String COL_RELEASE ="release";
+    public static final String COL_OS ="os";
+    public static final String COL_VERSION ="version";
+    public static final String COL_PROCESSOR ="processor";
+    public static final String COL_MEMORY ="memory";
+    public static final String COL_STORAGE ="storage";
+    public static final String COL_WEIGHT ="weight";
+
+
+    public static final int PHONES = 1;
+    public static final int PHONE_ID = 2;
+    public static final String PROVIDER_NAME ="com.jpnouchi.provider.Phones";
 	
 	
 	
